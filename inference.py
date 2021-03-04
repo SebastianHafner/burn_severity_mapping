00f5d98e9ab862d3bb3424642d712ffa457be656
SebastianHafner <hafnersailing@gmail.com>
2021-03-04 19:17:41 +0100

    whatever

--- inference.py
+++ inference.py
@@ -90,9 +90,9 @@ def site_label(config_name: str, site: str):
 if __name__ == '__main__':
     config_name = 'dnbr'
     sites = ['elephanthill2018aoi1', 'elephanthill2018aoi2', 'elephanthill2018aoi3', 'elephanthill2018aoi4',
-             'fagelsjo', 'ljusdals', 'storbrattan', 'trangslet']
-    # for site in sites:
-    #     site_inference(config_name, site)
-    labeled_sites = ['elephanthill', 'fagelsjo', 'trangslet']
-    for site in labeled_sites:
-        site_label(config_name, site)
+             'fagelsjo', 'ljusdals', 'storbrattan', 'trangslet', 'elephanthill']
+    for site in sites:
+        site_inference(config_name, site)
+    # labeled_sites = ['fagelsjo', 'trangslet']
+    # for site in labeled_sites:
+    #     site_label(config_name, site)
